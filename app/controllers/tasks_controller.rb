9949a48ede91f20173933efe12084cbb2c04c126
Michael Wong <mail@michaelwongsilversmith.com>
2018-10-03 11:09:00 +0100

    routes being generated

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,25 @@
 class TasksController < ApplicationController
+  # CRUD
+  # list all tasks
+  def index
+    @tasks = Task.all
+  end
+  # view details of a specific task
+  def show
+    @task = Task.find(params[:id])
+  end
+  # add a task
+  def create
+    @task = Task.create(params[:id])
+    redirect_to tasks_path
+  end
+  # edit a task
+  def update
+    @task = Task.update(params[:id])
+  end
+  # remove a task
+  def destory
+    @task.destory
+    redirect_to tasks_paths
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/show', to: 'tasks#show'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id/edit', to: 'tasks#edit'
 end
